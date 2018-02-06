3c28599f3f77cd03eea25aeac5d140ce27097a9b
tianbohao <tianbohao@boco.com.cn>
2018-02-06 16:21:32 +0800

    项目初始化

---
+++ tank-face/src/main/java/cn/hao/tankface/TankFaceApplication.java
@@ -0,0 +1,12 @@
+package cn.hao.tankface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TankFaceApplication.class, args);
+	}
+}
